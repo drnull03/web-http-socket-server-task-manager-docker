3f141622114ef85e2b08a933c3ff405dcfb06c9f
drnull03 <diaa.251202@gmail.com>
2025-11-14 19:52:10 +0100

    Added note for parallel computing instructor

---
+++ VERY_IMPORTANT_NOTE.md
@@ -0,0 +1,5 @@
+Dear Parallel Computing Instructor,
+
+Please note that the linked article, "https://levelup.gitconnected.com/unveiling-two-dynamic-multi-threaded-web-servers-for-task-execution-62644e78a04a", is behind a paywall on the Medium platform.
+
+Since Medium frequently restricts access to its high-quality content, it creates an obstacle for students who need to read the material. For future assignments, could you please provide the article using an alternative method that ensures unrestricted student access?
--- src/WebServerHttp.java
+++ src/WebServerHttp.java
@@ -6,11 +6,17 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
 public class WebServerHttp {
-
+    //port 8000
     private static final int PORT = 8000;
 
+
+
+    //main function (thread) running the server
     public static void main(String[] args) throws IOException {
+        // creating server on a socket connection on port 8000
         HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        //this line add context to the server   
+        //probably most important line we will see as we reverse engineer this shit
         server.createContext("/", new RequestsHandlerHttp());
 
         //server.setExecutor(Executors.newFixedThreadPool(1));
